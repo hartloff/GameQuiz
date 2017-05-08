44b014e5ee2941a348558d4c5a2cc0111f9f32c4
jessehartloff <hartloff@buffalo.edu>
2017-05-08 14:48:11 -0400

    initial commit

---
+++ src/GameQuiz.java
@@ -0,0 +1,116 @@
+
+public class GameQuiz{
+
+
+    public static char Q1(){
+//        1)	What type of ordering do stacks guarantee?
+//                a.	First In - First Out (FIFO)
+//                b.	Last In - First Out (LIFO)
+//                c.	Last In - Last Out (LILO)
+//                d.	Stacks don't preserve order
+
+        return 'e';
+    }
+
+
+    public static char Q2(){
+//        2)	Which of these operations do stacks NOT support?
+//                a.	pop()
+//                b.	peek()
+//                c.	sort()
+//                d.	push()
+
+        return 'e';
+    }
+
+
+    public static char Q3(){
+//        3)	When we transition from a Town/World Map to a Battle, what operation does the 
+//            Game Engine perform on the StateStack?
+//                a.	enqueue()
+//                b.	pop()
+//                c.	dequeue()
+//                d.	push()
+
+        return 'e';
+    }
+
+
+    public static char Q4(){
+//        4)	When we transition from a Battle to a Town/World Map, what operation does the 
+//          Game Engine perform on the StateStack?
+//                a.	enqueue()
+//                b.	pop()
+//                c.	dequeue()
+//                d.	push()
+
+        return 'e';
+    }
+
+
+    public static char Q5(){
+//        5)	How does the Game Engine choose which State to run?
+//                a.	It performs a pop() operation on the StateStack
+//                b.	It uses a switch case
+//                c.	It performs a peek() operation on the StateStack
+//                d.	It uses a random number generator
+
+        return 'e';
+    }
+
+
+    public static char Q6(){
+//        6)	When do we specifically add the Player to the Battle queue?
+//                a.	After the player's timer fires but before the Player make a decision
+//                b.	After the player's timer fires and after the player makes a decision
+//                c.	When the Game Engine performs push() on the Battle State
+//                d.	When the Steam Summer Sale Starts (50% off!)
+
+        return 'e';
+    }
+
+
+    public static char Q7(){
+//        7)	What type of ordering does a queue guarantee?
+//                a.	First In - First Out (FIFO)
+//                b.	Last In - First Out (LIFO)
+//                c.	First In - Last Out (FILO)
+//                d.	Queues don't preserve order
+
+        return 'e';
+    }
+
+
+    public static char Q8(){
+//        8)	TRUE/FALSE - It is easy to look up <key, value> pairs by value in a Dictionary
+//                a.	True
+//                b.	False
+
+        return 'e';
+    }
+
+
+    public static char Q9(){
+//        9)	What type of operation is used to add new items to the Player's Inventory 
+//           ](using a Dictionary)?
+//                a.	push()
+//                b.	enqueue()
+//                c.	put()
+//                d.	store()
+
+        return 'e';
+    }
+
+
+    public static char Q10(){
+//        10)   Why is Java such a fantastic programming language?
+//                a.	Because it is named after coffee
+//                b.	Because the language is not overly complicated
+//                c.	Because the language is highly portable
+//                d.	I have no idea, I'm only here for community points
+//                e.All of the above (except d)
+
+        return 'e';
+    }
+
+}
